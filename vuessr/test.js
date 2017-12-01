d1b56a4e0ed5f667f75aaf7ff5c6288a96730c76
SuAutumn <909153789@qq.com>
2017-12-01 10:24:55 +0800

    test for Object.defineProperty

--- vuessr/test.js
+++ vuessr/test.js
@@ -1,8 +1,7 @@
-var data = {
-  value: 1
-}
+'use strict'
+var data = {}
 
-let bValue = 1
+var bValue = 1
 Object.defineProperty(data, 'value', {
   // value: 1,
   get: () => {
@@ -12,11 +11,19 @@ Object.defineProperty(data, 'value', {
     bValue = newVal
     return bValue
   },
-  enumerable: true, // 可枚举
-  configurable: false // 可设置
+  // default value
+  // enumerable: false,
+  // configurable: false,
+  // writable: false,
+  // for (i in data) {
+  //   console.log(i)
+  // }
+  configurable: false, // 特性表示对象的属性是否可以被删除，以及除writable特性外的其他特性是否可以被修改。
+  // detail: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+  enumerable: true // 可枚举
 })
 
 console.log(data.value)
 data.value = 123
-delete data.value
+// delete data.value
 console.log(data.value)
